05b5d7f4cd0361123de37bfd8df6143f60a33405
yourwayisright <90473845+yourwayisright@users.noreply.github.com>
2021-10-17 10:06:21 +0300

    Update homework_9.py

--- homework_9.py
+++ homework_9.py
@@ -1,1 +1,13 @@
+user_answer = 'no'
+counter = 0
 
+
+for i in range (1, 101):
+    counter += 1
+    if user_answer == 'no':
+        print(f'Is {i} guessed number? yes/no')
+        received_answer = input()
+        user_answer = received_answer
+    else:
+        print(f"The number was guessed with {counter-1} attempts!")
+        break
