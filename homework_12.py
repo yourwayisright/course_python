f219101147b086301de94a10409a9f0c39f6206b
yourwayisright <90473845+yourwayisright@users.noreply.github.com>
2021-10-29 11:13:35 +0300

    Update homework_12.py

--- homework_12.py
+++ homework_12.py
@@ -1,20 +1,19 @@
 class Vector:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = int(x1)
-        self.y1 = int(y1)
-        self.x2 = int(x2)
-        self.y2 = int(y2)
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
 
-    def summarize (self):
-        result = (self.x1 + self.x2, self.y1 + self.y2)
+
+    def __add__(self, other):
+        result = (self.x + other.x, self.y + other.y)
         return result
 
-    def substraction (self):
-        result = (self.x1 - self.x2, self.y1 - self.y2)
+    def __sub__(self,other):
+        result = (self.x - other.x, self.y - other.y)
         return result
 
-    def multiplication(self):
-        result = (self.x1 * self.x2,self.y1 * self.x2)
+    def __mul__(self,other):
+        result = (self.x * int(other), self.y * int(other))
         return result
 
 while True:
@@ -24,20 +23,22 @@ while True:
     if command == 'summ':
         coordinate1 = input('Please input coordinates of the first vector: x y ').split()
         coordinate2 = input('Please input coordinates of the second vector: x y ').split()
-        exemplar = Vector(x1=coordinate1[0], y1=coordinate1[1], x2=coordinate2[0], y2=coordinate2[1])
-        result = Vector.summarize(exemplar)
+        exemplar1 = Vector(x=coordinate1[0], y=coordinate1[1])
+        exemplar2 = Vector(x=coordinate2[0], y=coordinate2[1])
+        result = exemplar1+exemplar2
         print(result)
 
     if command == 'substr':
         coordinate1 = input('Please input coordinates of the first vector: x y ').split()
         coordinate2 = input('Please input coordinates of the second vector: x y ').split()
-        exemplar = Vector(x1=coordinate1[0], y1=coordinate1[1], x2=coordinate2[0], y2=coordinate2[1])
-        result = Vector.substraction(exemplar)
+        exemplar1 = Vector(x=coordinate1[0], y=coordinate1[1])
+        exemplar2 = Vector(x=coordinate2[0], y=coordinate2[1])
+        result = exemplar1-exemplar2
         print(result)
 
     if command == 'multi':
             coordinate1 = input('Please input coordinates of the first vector: x y ').split()
             multiplicatior = input('Please input multiplicator: ')
-            exemplar = Vector(x1=coordinate1[0], y1=coordinate1[1], x2=multiplicatior[0], y2=1)
-            result = Vector.multiplication(exemplar)
-            print(result)                     
+            exemplar1 = Vector(x=coordinate1[0], y=coordinate1[1])
+            result = exemplar1*multiplicatior
+            print(result)
